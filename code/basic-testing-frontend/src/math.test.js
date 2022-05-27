a18cddf177cc31a3fd6a03d4b82f6ac6fd44ebe5
Allen <allenyuan@aslcn.com.cn>
2022-05-27 17:15:07 +0800

    test for other case

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -1,19 +1,34 @@
-import {
-    add
-} from "./math";
-import {
-    test,
-    expect
-} from "vitest";
+import { add } from "./math";
+import { test, expect } from "vitest";
 
-test('should summarize all number value in an array', () => {
-    // Arrange
-    const numbers = [10, 20, 30];
+test("should summarize all number value in an array", () => {
+  // Arrange
+  const numbers = [10, 20, 30];
 
-    // Act
-    const result = add(numbers);
+  // Act
+  const result = add(numbers);
 
-    // Assert
-    const expected = 60
-    expect(result).toBe(expected)
-})
+  // Assert
+  const expected = 60;
+  expect(result).toBe(expected);
+});
+
+test("should be return NaN if the array include an invalid number", () => {
+  const numbers = ["1", "allen"];
+
+  const result = add(numbers);
+
+  expect(result).toBeNaN();
+});
+
+test("should be correct sum even thought the array include string number", () => {
+  const numbers = ["1", 2];
+
+  const result = add(numbers);
+
+  const expected = numbers.reduce((prev, next) => {
+    return +prev + +next;
+  }, 0);
+
+  expect(result).toBe(expected);
+});
