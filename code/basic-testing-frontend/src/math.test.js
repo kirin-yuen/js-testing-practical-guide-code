4757a3057f6045468a92a115a93be39205579178
Allen <allenyuan@aslcn.com.cn>
2022-06-02 16:25:38 +0800

    error test

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -32,3 +32,28 @@ test("should be correct sum even thought the array include string number", () =>
 
   expect(result).toBe(expected);
 });
+
+test("should return 0 if an empty array passed", () => {
+  // Arrange
+  const numbers = [];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  expect(result).toBe(0);
+});
+
+test("should throw error if no argument passed", () => {
+  // vitest 会自动执行
+  expect(add).toThrowError();
+});
+
+test("should throw error if multiple argument passed", () => {
+  const resultFn = () => {
+    add(1, 2);
+  };
+
+  // 可以指定抛错内容是否匹配
+  expect(resultFn).toThrowError(/iterable/);
+});
